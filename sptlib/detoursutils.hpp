5e34048971684113c1c3197946fbf5efa69e00f8
Ivan Molodetskikh <yalterz@gmail.com>
2014-09-02 20:57:47 +0400

    Deleted the extra stuff.

--- sptlib/detoursutils.hpp
+++
@@ -1,5 +0,0 @@
-#include "sptlib-stdafx.hpp"
-#pragma once
-
-void AttachDetours( const std::wstring &moduleName, unsigned int argCount, ... );
-void DetachDetours( const std::wstring &moduleName, unsigned int argCount, ... );
