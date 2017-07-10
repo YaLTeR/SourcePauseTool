dd079082538bd3c91141245fd02608d553f72052
TheProJamer <theprogamerwithaj@gmail.com>
2017-07-11 08:29:36 +0930

    Name changes

--- spt/OrangeBox/cvars.hpp
+++ spt/OrangeBox/cvars.hpp
@@ -31,7 +31,7 @@ extern ConVar _y_spt_afterframes_reset_on_server_activate;
 extern ConVar _y_spt_pitchspeed;
 extern ConVar _y_spt_yawspeed;
 extern ConVar _y_spt_force_90fov;
-extern ConVar _y_spt_glitchless_bhop_enabled;
+extern ConVar _y_spt_glitchless_bhop;
 
 extern ConVar *_viewmodel_fov;
 extern ConVar *_sv_accelerate;
