cd43b5f38c862f682699e492e82a7fe6bbbc577d
TheProJamer <theprogamerwithaj@gmail.com>
2017-08-24 19:39:27 +0930

    Update ClientDLL.hpp

--- spt/OrangeBox/modules/ClientDLL.hpp
+++ spt/OrangeBox/modules/ClientDLL.hpp
@@ -1,69 +1,110 @@
 #include "..\..\stdafx.hpp"
 #pragma once
 
+#include <vector>
+
 #include <SPTLib\IHookableNameFilter.hpp>
 
 using std::uintptr_t;
 using std::size_t;
 
-typedef bool(__cdecl *_SV_ActivateServer) ();
-typedef void(__fastcall *_FinishRestore) (void* thisptr, int edx);
-typedef void(__fastcall *_SetPaused) (void* thisptr, int edx, bool paused);
-typedef void(__cdecl *__Host_RunFrame) (float time);
-typedef void(__cdecl *__Host_RunFrame_Input) (float accumulated_extra_samples, int bFinalTick);
-typedef void(__cdecl *__Host_RunFrame_Server) (int bFinalTick);
-typedef void(__cdecl *_Cbuf_Execute) ();
-typedef void*(__cdecl *_Cbuf_GetCommandBuffer) ();
-typedef void(__cdecl *_Cbuf_AddText)(void* command_buffer, const char* text, int unknown);
+typedef void(__cdecl *_DoImageSpaceMotionBlur) (void* view, int x, int y, int w, int h);
+//typedef bool(__fastcall *_CheckJumpButton) (void* thisptr, int edx);
+typedef void(__stdcall *_HudUpdate) (bool bActive);
+typedef int(__fastcall *_GetButtonBits) (void* thisptr, int edx, int bResetState);
+typedef void(__fastcall *_AdjustAngles) (void* thisptr, int edx, float frametime);
+typedef void(__fastcall *_CreateMove) (void* thisptr, int edx, int sequence_number, float input_sample_frametime, bool active);
+typedef void(__fastcall *_CViewRender__OnRenderStart) (void* thisptr, int edx);
+typedef void*(__cdecl *_GetLocalPlayer) ();
+typedef void*(__fastcall *_GetGroundEntity) (void* thisptr, int edx);
+typedef void(__fastcall *_CalcAbsoluteVelocity) (void* thisptr, int edx);
 
-class EngineDLL : public IHookableNameFilter
+typedef struct
+{
+	long long int framesLeft;
+	std::string command;
+} afterframes_entry_t;
+
+typedef struct 
+{
+	float angle;
+	bool set;
+} angset_command_t;
+
+class ClientDLL : public IHookableNameFilter
 {
 public:
-	EngineDLL() : IHookableNameFilter( { L"engine.dll" } ) {};
+	ClientDLL() : IHookableNameFilter({ L"client.dll" }) {};
 	virtual void Hook(const std::wstring& moduleName, HMODULE hModule, uintptr_t moduleStart, size_t moduleLength);
 	virtual void Unhook();
 	virtual void Clear();
 
-	static bool __cdecl HOOKED_SV_ActivateServer();
-	static void __fastcall HOOKED_FinishRestore(void* thisptr, int edx);
-	static void __fastcall HOOKED_SetPaused(void* thisptr, int edx, bool paused);
-	static void __cdecl HOOKED__Host_RunFrame(float time);
-	static void __cdecl HOOKED__Host_RunFrame_Input(float accumulated_extra_samples, int bFinalTick);
-	static void __cdecl HOOKED__Host_RunFrame_Server(int bFinalTick);
-	static void __cdecl HOOKED_Cbuf_Execute();
-	bool __cdecl HOOKED_SV_ActivateServer_Func();
-	void __fastcall HOOKED_FinishRestore_Func(void* thisptr, int edx);
-	void __fastcall HOOKED_SetPaused_Func(void* thisptr, int edx, bool paused);
-	void __cdecl HOOKED__Host_RunFrame_Func(float time);
-	void __cdecl HOOKED__Host_RunFrame_Input_Func(float accumulated_extra_samples, int bFinalTick);
-	void __cdecl HOOKED__Host_RunFrame_Server_Func(int bFinalTick);
-	void __cdecl HOOKED_Cbuf_Execute_Func();
+	static void __cdecl HOOKED_DoImageSpaceMotionBlur(void* view, int x, int y, int w, int h);
+	//static bool __fastcall HOOKED_CheckJumpButton(void* thisptr, int edx);
+	static void __stdcall HOOKED_HudUpdate(bool bActive);
+	static int __fastcall HOOKED_GetButtonBits(void* thisptr, int edx, int bResetState);
+	static void __fastcall HOOKED_AdjustAngles(void* thisptr, int edx, float frametime);
+	static void __fastcall HOOKED_CreateMove (void* thisptr, int edx, int sequence_number, float input_sample_frametime, bool active);
+	static void __fastcall HOOKED_CViewRender__OnRenderStart(void* thisptr, int edx);
+	void __cdecl HOOKED_DoImageSpaceMotionBlur_Func(void* view, int x, int y, int w, int h);
+	//bool __fastcall HOOKED_CheckJumpButton_Func(void* thisptr, int edx);
+	void __stdcall HOOKED_HudUpdate_Func(bool bActive);
+	int __fastcall HOOKED_GetButtonBits_Func(void* thisptr, int edx, int bResetState);
+	void __fastcall HOOKED_AdjustAngles_Func(void* thisptr, int edx, float frametime);
+	void __fastcall HOOKED_CreateMove_Func(void* thisptr, int edx, int sequence_number, float input_sample_frametime, bool active);
+	void __fastcall HOOKED_CViewRender__OnRenderStart_Func(void* thisptr, int edx);
 
-	float GetTickrate() const;
-	void SetTickrate(float value);
+	void DelayAfterframesQueue(int delay);
+	void AddIntoAfterframesQueue(const afterframes_entry_t& entry);
+	void ResetAfterframesQueue();
 
-	int Demo_GetPlaybackTick() const;
-	int Demo_GetTotalTicks() const;
-	bool Demo_IsPlayingBack() const;
-	bool Demo_IsPlaybackPaused() const;
+	void PauseAfterframesQueue() { afterframesPaused = true; }
+	void ResumeAfterframesQueue() { afterframesPaused = false; }
+
+	void EnableDuckspam()  { duckspam = true; }
+	void DisableDuckspam() { duckspam = false; }
+
+	void SetPitch(float pitch) { setPitch.angle = pitch; setPitch.set = true; }
+	void SetYaw(float yaw)     { setYaw.angle   = yaw;   setYaw.set   = true; }
 
 protected:
-	_SV_ActivateServer ORIG_SV_ActivateServer;
-	_FinishRestore ORIG_FinishRestore;
-	_SetPaused ORIG_SetPaused;
-	__Host_RunFrame ORIG__Host_RunFrame;
-	__Host_RunFrame_Input ORIG__Host_RunFrame_Input;
-	_Cbuf_GetCommandBuffer Cbuf_GetCommandBuffer;
-	_Cbuf_AddText Cbuf_AddText;
-	__Host_RunFrame_Server ORIG__Host_RunFrame_Server;
-	_Cbuf_Execute ORIG_Cbuf_Execute;
+	_DoImageSpaceMotionBlur ORIG_DoImageSpaceMorionBlur;
+	//_CheckJumpButton ORIG_CheckJumpButton;
+	_HudUpdate ORIG_HudUpdate;
+	_GetButtonBits ORIG_GetButtonBits;
+	_AdjustAngles ORIG_AdjustAngles;
+	_CreateMove ORIG_CreateMove;
+	_CViewRender__OnRenderStart ORIG_CViewRender__OnRenderStart;
+	_GetLocalPlayer GetLocalPlayer;
+	_GetGroundEntity GetGroundEntity;
+	_CalcAbsoluteVelocity CalcAbsoluteVelocity;
 
-	void* pGameServer;
-	bool* pM_bLoadgame;
-	bool shouldPreventNextUnpause;
-	float* pIntervalPerTick;
-	float* pHost_Frametime;
-	int* pM_State;
-	int* pM_nSignonState;
-	void** pDemoplayer;
+	uintptr_t* pgpGlobals;
+	ptrdiff_t offM_pCommands;
+	ptrdiff_t offForwardmove;
+	ptrdiff_t offSidemove;
+	ptrdiff_t offMaxspeed;
+	ptrdiff_t offFlags;
+	ptrdiff_t offAbsVelocity;
+	ptrdiff_t offDucking;
+	ptrdiff_t offDuckJumpTime;
+	ptrdiff_t offServerSurfaceFriction;
+	ptrdiff_t offServerPreviouslyPredictedOrigin;
+public:
+	ptrdiff_t offServerAbsOrigin;
+protected:
+	uintptr_t pCmd;
+
+	bool tasAddressesWereFound;
+
+	std::vector<afterframes_entry_t> afterframesQueue;
+	bool afterframesPaused = false;
+
+	bool duckspam;
+	angset_command_t setPitch, setYaw;
+	bool forceJump;
+
+	void OnFrame();
+
+	int afterframesDelay;
 };
