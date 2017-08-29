74dbc371835918292e211637c27bebbd7c8bc7b4
TheProJamer <theprogamerwithaj@gmail.com>
2017-08-29 12:48:49 +0930

    More tiring OE strafe changes I haven't tested

--- spt/strafestuff.cpp
+++ spt/strafestuff.cpp
@@ -280,7 +280,7 @@ bool Strafe(PlayerData& player, const MovementVars& vars, bool onground, bool ju
 			}
 			else {
 				const Vector vel = player.Velocity;
-				out.yaw = NormalizeDeg(NormalizeRad(atan2(vel.y, vel.x)) + (90 - (asin2((((sin (NormalizeRad(atan2(vel.y, vel.x)))*(player.Velocity+flSpeedBoostPerc))/sin 90)*sin 90)/(x + flSpeedBoostPerc))))
+				out.yaw = NormalizeDeg(NormalizeRad(atan2(vel.y, vel.x)) + (90 - (asin2((((sin (NormalizeRad(atan2(vel.y, vel.x)))*(player.Velocity+flSpeedBoostPerc))/sin 90)*sin 90)/(((sin (NormalizeRad(atan2(vel.y, vel.x)))*(player.Velocity+flSpeedBoostPerc))/sin 90) + flSpeedBoostPerc))))
 			#endif
 		}
 		out.Forward = false;
