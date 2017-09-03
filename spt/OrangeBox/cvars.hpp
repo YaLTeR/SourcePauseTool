a4958652a294e57ae1f73081f7a26a47c0c48d7c
TheProJamer <theprogamerwithaj@gmail.com>
2017-09-03 15:12:29 +0930

    Update cvars.hpp

--- spt/OrangeBox/cvars.hpp
+++ spt/OrangeBox/cvars.hpp
@@ -5,7 +5,6 @@
 extern ConVar y_spt_pause;
 extern ConVar y_spt_motion_blur_fix;
 extern ConVar y_spt_autojump;
-extern ConVar y_spt_autojump_ensure_legit;
 extern ConVar y_spt_additional_abh;
 extern ConVar y_spt_stucksave;
 extern ConVar y_spt_pause_demo_on_tick;
