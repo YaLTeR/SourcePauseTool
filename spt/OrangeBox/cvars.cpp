05b7df078b8ea74f6626ffbe0716c0b9f7dffb72
TheProJamer <theprogamerwithaj@gmail.com>
2017-09-03 15:11:29 +0930

    Update cvars.cpp

--- spt/OrangeBox/cvars.cpp
+++ spt/OrangeBox/cvars.cpp
@@ -3,7 +3,6 @@
 ConVar y_spt_pause("y_spt_pause", "1", FCVAR_ARCHIVE);
 ConVar y_spt_motion_blur_fix("y_spt_motion_blur_fix", "0");
 ConVar y_spt_autojump("y_spt_autojump", "0", FCVAR_ARCHIVE);
-ConVar y_spt_autojump_ensure_legit("y_spt_autojump_ensure_legit", "0", FCVAR_ARCHIVE);
 ConVar y_spt_additional_abh("y_spt_additional_abh", "0", FCVAR_ARCHIVE);
 ConVar y_spt_stucksave("y_spt_stucksave", "");
 ConVar y_spt_pause_demo_on_tick(
