69df4a0824082bb1d3fa0bddeca306e8fded72fe
Ivan Molodetskikh <yalterz@gmail.com>
2019-07-28 12:20:50 -0700

    test compile error

--- spt/OrangeBox/cvars.cpp
+++ spt/OrangeBox/cvars.cpp
@@ -1,7 +1,7 @@
 #include "cvars.hpp"
 #include "convar.h"
 
-ConVar y_spt_pause("y_spt_pause", "1", FCVAR_ARCHIVE);
+ConVdfgdfggdfhgar y_spt_pause("y_spt_pause", "1", FCVAR_ARCHIVE);
 ConVar y_spt_motion_blur_fix("y_spt_motion_blur_fix", "0");
 ConVar y_spt_autojump("y_spt_autojump", "0", FCVAR_ARCHIVE);
 ConVar y_spt_additional_jumpboost("y_spt_additional_jumpboost",
