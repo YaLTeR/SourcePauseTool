9a1f0b00c0594b01954e35a473a7f2c1cc27ff25
Jukspa <jukspaz@gmail.com>
2020-02-13 00:13:43 +0200

    Changed the default value for y_spt_pause from 1 to 0

--- spt/OrangeBox/cvars.cpp
+++ spt/OrangeBox/cvars.cpp
@@ -2,7 +2,7 @@
 
 #include "convar.h"
 
-ConVar y_spt_pause("y_spt_pause", "1", FCVAR_ARCHIVE);
+ConVar y_spt_pause("y_spt_pause", "0", FCVAR_ARCHIVE);
 ConVar y_spt_motion_blur_fix("y_spt_motion_blur_fix", "0");
 ConVar y_spt_autojump("y_spt_autojump", "0", FCVAR_ARCHIVE);
 ConVar y_spt_additional_jumpboost("y_spt_additional_jumpboost",
