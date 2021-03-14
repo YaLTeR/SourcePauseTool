6d6e72e4e101e6b81f9c012ced6be4dc5da047a7
SmileyAG <58108407+SmileyAG@users.noreply.github.com>
2021-03-14 23:25:09 +0400

    Extended the patterns.

--- spt/OrangeBox/patterns.hpp
+++ spt/OrangeBox/patterns.hpp
@@ -156,7 +156,7 @@ namespace patterns
 		    "missinginfo1_4_7",
 		    "55 8B EC 83 EC 08 89 4D F8 A1 ?? ?? ?? ?? 8B 48 10 89 4D FC E8 ?? ?? ?? ?? D9 45 FC 83 EC 08 DD 1C 24",
 		    "hl1movement",
-		    "55 8B EC 51 A1 ?? ?? ?? ?? 56 83 EC 08");
+		    "55 8B EC 51 A1 ?? ?? ?? ?? 56 83 EC 08 F3 0F 10 40 10 F3 0F 11 45 FC 0F 5A C0 F2 0F 11 04 24 E8 ?? ?? ?? ?? 8B C8");
 		PATTERNS(
 		    GetButtonBits,
 		    "5135",
@@ -311,7 +311,7 @@ namespace patterns
 		    "missinginfo1_4_7",
 		    "55 8B EC 83 EC 44 56 89 4D D0 8B 45 D0 8B 48 08 81 C1 ?? ?? ?? ?? E8 ?? ?? ?? ?? 0F B6 C8 85 C9",
 		    "te120",
-		    "55 8B EC 83 EC 20 56 8B F1 8B 4E ??",
+		    "55 8B EC 83 EC 20 56 8B F1 8B 4E 04 80 B9 48 0A 00 00 00 74 0E 8B 76 08 83 4E 28 02 32 C0 5E 8B E5 5D C3 F3 0F 10 89 B4 0D 00 00",
 		    "missinginfo1_6",
 		    "55 8B EC 83 EC 1C 56 8B F1 8B 4E 04 80 B9 ?? ?? ?? ?? ?? 74 0E 8B 76 08 83 4E 28 02 32 C0 5E 8B E5");
 		PATTERNS(
