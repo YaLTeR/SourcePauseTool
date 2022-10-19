334335ed5ce0e4771bc10f20a905890d0042893d
evanlin96069 <72735402+evanlin96069@users.noreply.github.com>
2022-10-19 14:50:56 -0400

    Added y_spt_release_mutex.

---
+++ spt/features/multi_instance.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#ifdef _WIN32
+#include "..\feature.hpp"
+#include <Windows.h>
+
+CON_COMMAND(y_spt_release_mutex, "Releases \"hl2_singleton_mutex\" to enable running multiple instances.")
+{
+	HANDLE handle = OpenMutexA(SYNCHRONIZE, false, "hl2_singleton_mutex");
+	if (handle)
+	{
+		if (ReleaseMutex(handle))
+			Msg("Released hl2_singleton_mutex. You can start another instance now.\n");
+		else
+			Warning("Failed to release hl2_singleton_mutex.\n");
+		CloseHandle(handle);
+	}
+	else
+		Warning("Failed to obtain hl2_singleton_mutex handle.\n");
+}
+
+// y_spt_release_mutex
+class MultiInstance : public FeatureWrapper<MultiInstance>
+{
+public:
+protected:
+	virtual bool ShouldLoadFeature() override;
+
+	virtual void InitHooks() override;
+
+	virtual void LoadFeature() override;
+
+	virtual void UnloadFeature() override;
+};
+
+static MultiInstance spt_multi_instance;
+
+bool MultiInstance::ShouldLoadFeature()
+{
+	return true;
+}
+
+void MultiInstance::InitHooks() {}
+
+void MultiInstance::LoadFeature()
+{
+	InitCommand(y_spt_release_mutex);
+}
+
+void MultiInstance::UnloadFeature() {}
+
+#endif
