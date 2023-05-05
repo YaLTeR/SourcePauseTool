b09ba36d9ab4d8bafda786ac3b386a60e82b3dd0
UncraftedName <uncraftedname@gmail.com>
2023-05-05 12:10:37 -0700

    Sort datamap fields by offset

--- spt/utils/datamap_wrapper.cpp
+++ spt/utils/datamap_wrapper.cpp
@@ -34,42 +34,47 @@ namespace utils
 
 	void DatamapWrapper::ExploreOffsets()
 	{
-		auto map = _serverMap;
-		if (map)
+		for (int i = 0; i < 2; i++)
 		{
-			Msg("%s server map:\n", map->dataClassName);
-			ExploreOffsetsHelper(map, 0, "");
-			Msg("\n");
-		}
-		map = _clientMap;
-		if (map)
-		{
-			Msg("%s client map:\n", map->dataClassName);
-			ExploreOffsetsHelper(map, 0, "");
+			auto map = i == 0 ? _serverMap : _clientMap;
+			if (!map)
+				continue;
+
+			std::vector<std::pair<int, std::string>> vec;
+			ExploreOffsetsHelper(vec, map, 0, "");
+			std::sort(vec.begin(), vec.end());
+
+			Msg("%s %s map:\n", map->dataClassName, i == 0 ? "server" : "client");
+			for (const auto& [off, str] : vec)
+				Msg("\t%s : %d\n", str.c_str(), off);
 			Msg("\n");
 		}
 	}
 
-	void DatamapWrapper::ExploreOffsetsHelper(datamap_t* map, int prefixOffset, std::string prefix)
+	void DatamapWrapper::ExploreOffsetsHelper(std::vector<std::pair<int, std::string>>& outVec,
+	                                          datamap_t* map,
+	                                          int prefixOffset,
+	                                          const std::string& prefix)
 	{
 		if (map->baseMap)
-			ExploreOffsetsHelper(map->baseMap, prefixOffset, prefix);
+			ExploreOffsetsHelper(outVec, map->baseMap, prefixOffset, prefix);
 
 		for (int i = 0; i < map->dataNumFields; ++i)
 		{
-			auto typedescription = map->dataDesc[i];
+			auto& typedescription = map->dataDesc[i];
 
+			// ignore input & outputs
 			switch (typedescription.fieldType)
 			{
-				// Some weird stuff so ignore it
 			case FIELD_VOID:
-			case FIELD_CUSTOM:
 			case FIELD_FUNCTION:
-			case FIELD_TYPECOUNT:
+			case FIELD_INPUT:
 				continue;
 			default:
 				break;
 			}
+			if (typedescription.flags & (FTYPEDESC_INPUT | FTYPEDESC_OUTPUT))
+				continue;
 
 			int offset = typedescription.fieldOffset[0] + prefixOffset;
 			std::string prefixedName = prefix + typedescription.fieldName;
@@ -79,10 +84,10 @@ namespace utils
 				// Datamaps for "embedded" members (read: struct)
 				// The child fields will be named parentField.childField in the offset map
 			case FIELD_EMBEDDED:
-				ExploreOffsetsHelper(typedescription.td, offset, prefixedName + ".");
+				ExploreOffsetsHelper(outVec, typedescription.td, offset, prefixedName + ".");
 				break;
 			default:
-				Msg("\t%s : %d\n", prefixedName.c_str(), offset);
+				outVec.emplace_back(offset, prefixedName.c_str());
 				break;
 			}
 		}
